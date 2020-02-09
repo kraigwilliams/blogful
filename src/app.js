d43d12b31a1adbdcb7d4d13f211009cee2adfb47
kraigwilliams <krgwilliams@gmail.com>
2020-02-08 22:03:17 -0400

    setting up services

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors');
 const helmet = require('helmet')
 const morgan = require('morgan')
-
+const ArticlesService = require('./articles-service')
 const app = express()
 const { NODE_ENV } = require('./config')
 const morganOption = (NODE_ENV=== 'production')
@@ -13,8 +13,18 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+
+
 app.get('/',(req,res)=>{
 res.status(200).send("Hello World.")
+})
+
+app.get('/articles', (req,res,next)=>{
+    ArticlesService.getAllArticles()
+    .then(articles =>{
+        res.json(articles)
+    })
+    .catch(next)
 })
 
 app.use(function erroHandler(error, req,res,next){
