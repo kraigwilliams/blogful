dd05b060593f8ce1bf78bf8e8ec9525d2fbb29b5
kraigwilliams <krgwilliams@gmail.com>
2020-02-19 09:50:03 -0400

    switch workspace

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+//require('dotenv').config()
 const express = require('express')
 const cors = require('cors');
 const helmet = require('helmet')
@@ -21,11 +21,44 @@ res.status(200).send("Hello World.")
 
 app.get('/articles', (req,res,next)=>{
     const knexInstance = req.app.get('db')
+
     ArticlesService.getAllArticles(knexInstance)
     .then(articles =>{
-        res.json(articles)
+       res.json(articles )
+       
+        // res.json(articles.map(article =>({
+            
+        
+            
+        //     id:article.id,
+        //     title:article.title,
+
+        //     style:article.style,
+        //     content: article.content,
+        //     date_published: article.date_published
+        // })))
+    
+        // res.json(articles.map(article => {
+        //     let date_published = new Date(article.date_published)                 
+        //      return {           
+        //       id:article.id,
+        //       title:article.title,
+        //       style:article.style,
+        //       content: article.content,
+        //       date_published
+        //      }
+        //     }))
+
+
+
+
+
     })
     .catch(next)
+})
+
+app.get('/articles/:article_id', (req,res,next)=>{
+    res.json({'requested_id': req.params.article_id, this: 'should fail'})
 })
 
 app.use(function erroHandler(error, req,res,next){
--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,20 +1,24 @@
 const {expect} = require('chai')
 const knex = require('knex')
 const app = require('../src/app')
+const { makeArticlesArray } = require('./articles.fixtures');
 
-
-describe('Articles Endpoints', ()=>{
+describe.only('Articles Endpoints', ()=>{
 let db;
 before('make knex instance',()=>{
     db= knex({
         client:'pg',
         connection:process.env.TEST_DB_URL
     })
+    app.set('db', db)
 })
 
 after('disconnect from db',()=>db.destroy())
 
 before('clean the table', ()=> db('blogful_articles').truncate())
+afterEach('clean the table', ()=> db('blogful_articles').truncate())
+
+
 
 context('Given there are articles in the database', ()=>{
     const testArticles = [
@@ -47,16 +51,25 @@ context('Given there are articles in the database', ()=>{
                  content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam. Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam?'
                },
              ];
+             const testArts = makeArticlesArray()
 beforeEach('insert articles',()=>{
     return db
     .into('blogful_articles')
-    .insert(testArticles)
+    .insert(testArts)
 })
 it('Get /articles responds with 200 and all of the articles', ()=>{
     return supertest(app)
     .get('/articles')
-    .expect(200)
-    //TODO: add more assertions about the body
+    .expect(200, testArts)
+    
+})
+
+it.skip('Get /article/:article_id responds with 200 and the specified article', ()=>{
+const articleId =2
+const expectedArticle = testArticles[articleId-1]
+return supertest(app)
+.get(`/articles/${articleId}`)
+.expect(200 , expectedArticle)
 })
 
 })
